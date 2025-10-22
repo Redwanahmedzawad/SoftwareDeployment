796464a04e6cf3597d2d06249ecb12043e5fe0ce
Zawad <md.red1ahmedzawad@gmail.com>
2025-10-22 19:44:02 +1100

    Working But too simple

--- web/verisys-web/vite.config.ts
+++ web/verisys-web/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:8080", // local dev on host
+        target: "http://app:8080", // local dev on host
         changeOrigin: true,
       },
     },
