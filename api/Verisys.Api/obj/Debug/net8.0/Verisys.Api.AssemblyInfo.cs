a04b64f095389378a297d2674127bc2e81bfb450
H M Asfaq Ahmed Shihab <shihabasfak@gmail.com>
2025-10-24 13:21:24 +1100

    Last test

--- api/Verisys.Api/obj/Debug/net8.0/Verisys.Api.AssemblyInfo.cs
+++ api/Verisys.Api/obj/Debug/net8.0/Verisys.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Verisys.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aafa8aed24686944570c044e5e4aa17a9382dc2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4692f00c02309b5c79cfdc1dc435b724e17619dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Verisys.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Verisys.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
